5afb7b5e3f1327239e2895ba51bf287304377206
Emile Silvis <emile@silvis.co.za>
2013-06-06 17:23:01 +0200

    Force Fred's filters to continue on error

--- lib/photoforge/fred_filter.rb
+++ lib/photoforge/fred_filter.rb
@@ -1,7 +1,12 @@
 	module FredFilter
 
 	def apply(script,file,filename)
-		system "bash lib/fredscripts/#{script} #{file.path} public/temp/fred/#{filename}" 
+		begin
+			system "bash lib/fredscripts/#{script} #{file.path} public/temp/fred/#{filename}"
+		rescue
+			next
+		end 
 		open('public/temp/fred/' + filename).read 
+
 	end
 end
